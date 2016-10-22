319365b62b9665151a0ff5da9ce5d33924b6efee
chris-wong-1 <wong.christopher.s@gmail.com>
2016-10-22 12:10:48 -0700

    Configure env variables

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-TWILIO_SID = ENV['TWILIO_SID']
-TWILIO_TOKEN = ENV['TWILIO_TOKEN']
-MC_KEY_PASSWORD = ENV['MC_KEY_PASSWORD']
-MC_KEY_ALIAS = ENV['MC_KEY_ALIAS']
-MC_KEY = ENV['MC_KEY']
-MC_PARTNERID = ENV['MC_PARTNERID']
