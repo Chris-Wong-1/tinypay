bbe98bb4084b21cbd3b9cfa5f491923c05673258
Justincadburywong <justincadburywong@gmail.com>
2016-10-23 10:47:46 -0700

    change function to indivual functions

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,87 +7,127 @@ function animations(){
   $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/99875/screenshots/1805070/more-drib.gif' alt='mastercard'>")
 
   function loading(){
-    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Listening for payment</p>")
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Listening for payment</p>");
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        a();
+      }
   }
   setTimeout(loading, 7000);
 
-  $(window).keypress(function(e) {
-    if (e.which === 32) {
-      $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>TinyPay payment text received.</p>")
 
-      $(window).keypress(function(e) {
-        if (e.which === 32) {
-          debugger;
-          console.log("Im inside")
-            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying Sender and Recipient</p>")
+  function a(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>TinyPay payment text received.</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        b();
+      }
+    })
+  }
 
-            $(window).keypress(function(e) {
-              if (e.which === 32) {
-              $('.loader').html("<p class='animatedText'>Verified Sender and Recipient</p><br><img src='https://d13yacurqjgara.cloudfront.net/users/799964/screenshots/3004653/5.1_cc_to_safe.gif' alt='verified sender and recipient'>")
+  function b(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying Sender and Recipient</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        c();
+      }
+    })
+  }
 
-              $(window).keypress(function(e) {
-                if (e.which === 32) {
-                  $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user for payment amount... </p>")
+  function c(){
+    $('.loader').html("<p class='animatedText'>Verified Sender and Recipient</p><br><img src='https://d13yacurqjgara.cloudfront.net/users/799964/screenshots/3004653/5.1_cc_to_safe.gif' alt='verified sender and recipient'>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        d();
+      }
+    })
+  }
 
-                  $(window).keypress(function(e) {
-                    if (e.which === 32) {
-                      $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to reply with amount...</p>")
+  function d(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user for payment amount... </p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        e();
+      }
+    })
+  }
 
-                      $(window).keypress(function(e) {
-                        if (e.which === 32) {
-                            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Amount Received.</p>")
+  function e(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to reply with amount...</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        f();
+      }
+    })
+  }
 
-                            $(window).keypress(function(e) {
-                              if (e.which === 32) {
-                                $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user to verify with PIN...</p>")
+  function f(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Amount Received.</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        g();
+      }
+    })
+  }
 
-                                $(window).keypress(function(e) {
-                                    if (e.which === 32) {
-                                        $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to confirm verification with PIN...</p>")
+  function g(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user to verify with PIN...</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        h();
+      }
+    })
+  }
+  function h(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to confirm verification with PIN...</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        i();
+      }
+    })
+  }
+  function i(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>PIN received.</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        j();
+      }
+    })
+  }
 
-                                        $(window).keypress(function(e) {
-                                          if (e.which === 32) {
-                                            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>PIN received.</p>")
-                                            $(window).keypress(function(e) {
-                                            if (e.which === 32) {
-                                              $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying user with PIN.</p>")
-                                              $(window).keypress(function(e) {
-                                                if (e.which === 32) {
-                                                  $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/159981/screenshots/2345952/vcn.gif' alt='send'><br><p class='animatedText'>User verified.</p>")
-                                                  $(window).keypress(function(e) {
-                                                    if (e.which === 32) {
-                                                      $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/486050/screenshots/2832783/seamless-payments.gif' alt='send'><br><br><p class='animatedText'>Processing payment</p>")
-                                                    }
-                                                    $(window).keypress(function(e) {
-                                                      if (e.which === 32) {
-                                                        $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/760333/screenshots/2123456/lipco.gif' alt='send'><br><br><p class='animatedText'>Payment Complete!</p>")
-                                                      }
-                                                    })
-                                                    }
-                                                  })
-                                                }
-                                              })
-                                            }
-                                          })
+  function j(){
+    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying user with PIN.</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        k();
+      }
+    })
+  }
 
-                                          }
-                                        })
-                                    }
-                                  })
-                              }
-                            })
-                        }
-                      })
-                    }
-                  })
-                }
-              })
-            }
-          })
-        }
-      })
-    }
-  })
+  function k(){
+    $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/159981/screenshots/2345952/vcn.gif' alt='send'><br><p class='animatedText'>User verified.</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        l();
+      }
+    })
+  }
 
+  function l(){
+    $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/486050/screenshots/2832783/seamless-payments.gif' alt='send'><br><br><p class='animatedText'>Processing payment</p>")
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        m();
+      }
+    })
+  }
 
+  function m(){
+    $(window).keypress(function(e) {
+      if (e.which === 32) {
+        $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/760333/screenshots/2123456/lipco.gif' alt='send'><br><br><p class='animatedText'>Payment Complete!</p>")
+      }
+    })
+    setInterval(, 7000);
+  }
 }
