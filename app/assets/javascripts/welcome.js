dbd75199662b08318146c7cf4a40eb8f6153e6c7
hypatiah <danielle.hassid@gmail.com>
2016-10-22 20:29:26 -0700

    Add welcome.js file

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  showMastercardGif();
+})
+
+function showMastercardGif(){
+  $('#loader').html(<img src='https://d13yacurqjgara.cloudfront.net/users/99875/screenshots/1805070/more-drib.gif'>)
+}
