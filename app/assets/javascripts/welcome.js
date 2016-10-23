471dda78d77f82003709f64fe703dffba614d9b3
hypatiah <danielle.hassid@gmail.com>
2016-10-23 09:51:20 -0700

    Make animations clickable, error with parentheses

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,90 +1,93 @@
 $(document).ready(function(){
-  showMastercardGif();
+  animations();
 })
 
-function showMastercardGif(){
+function animations(){
   //showmastercardgif
   $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/99875/screenshots/1805070/more-drib.gif' alt='mastercard'>")
 
   function loading(){
     $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Listening for payment</p>")
   }
-  setTimeout(loading, 6500);
+  setTimeout(loading, 7000);
 
   $(window).keypress(function(e) {
     if (e.which === 32) {
       $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>TinyPay payment text received.</p>")
 
-
-      function verifyingSenderAndReciepient(){
-        $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying Sender and Recipient</p>")
-      }
-      setTimeout(verifyingSenderAndReciepient, 2000);
-
-      function verifiedSenderAndReciepient(){
-          $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/799964/screenshots/3004653/5.1_cc_to_safe.gif' alt='verify'><br><p class='animatedText'>Verified Sender and Recipient</p>")
-      }
-      setTimeout(verifiedSenderAndReciepient, 4000);
-
-
       $(window).keypress(function(e) {
         if (e.which === 32) {
-          $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user for payment amount... </p>")
+          debugger;
+          console.log("Im inside")
+            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying Sender and Recipient</p>")
 
-          function loading(){
-            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to reply with amount...</p>")
-          }
-          setTimeout(loading, 2000);
-          $(window).keypress(function(e) {
-            if (e.which === 32) {
-                $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Amount Received.</p>")
-                function askPin(){
-                  $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user to verify with PIN...</p>")
-                }
-                setTimeout(askPin, 2000);
-                function waitingPin(){
-                  $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to confirm verification with PIN...</p>")
-                }
-                setTimeout(waitingPin, 2000);
-              //amount received
-              //prompting user for pin verficiation
-              //pin verification received
-              //processing payment
-              //transaction successful
+            $(window).keypress(function(e) {
+              if (e.which === 32) {
+              $('.loader').html("<p class='animatedText'>Verified Sender and Recipient</p><br><img src='https://d13yacurqjgara.cloudfront.net/users/799964/screenshots/3004653/5.1_cc_to_safe.gif' alt='verified sender and recipient'>")
+
               $(window).keypress(function(e) {
                 if (e.which === 32) {
-                  $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>PIN received.</p>")
-
-                  function verifyingUser(){
-                    $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying user with PIN.</p>")
-                  }
-                  setTimeout(verifyingUser, 2000);
-
-                  function verifiedUser(){
-                    $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/159981/screenshots/2345952/vcn.gif' alt='send'><br><p class='animatedText'>User verified.</p>")
-                  }
-                  setTimeout(verifiedUser, 4000);
-
-                  function sendPayment(){
-                    $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/486050/screenshots/2832783/seamless-payments.gif' alt='send'><br><br><p class='animatedText'>Processing payment</p>")
-                  }
-                  setTimeout(sendPayment, 6000);
+                  $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user for payment amount... </p>")
 
                   $(window).keypress(function(e) {
                     if (e.which === 32) {
-                      $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/760333/screenshots/2123456/lipco.gif' alt='send'><br><br><p class='animatedText'>Payment Complete!</p>")
-                    }
-                  });
+                      $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to reply with amount...</p>")
 
+                      $(window).keypress(function(e) {
+                        if (e.which === 32) {
+                            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Amount Received.</p>")
+
+                            $(window).keypress(function(e) {
+                              if (e.which === 32) {
+                                $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Asking user to verify with PIN...</p>")
+
+                                $(window).keypress(function(e) {
+                                    if (e.which === 32) {
+                                        $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Waiting for user to confirm verification with PIN...</p>")
+
+                                        $(window).keypress(function(e) {
+                                          if (e.which === 32) {
+                                            $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>PIN received.</p>")
+                                            $(window).keypress(function(e) {
+                                            if (e.which === 32) {
+                                              $('.loader').html("<br><br><div id='spinner'></div><br><br><p class='animatedText'>Verifying user with PIN.</p>")
+                                              $(window).keypress(function(e) {
+                                                if (e.which === 32) {
+                                                  $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/159981/screenshots/2345952/vcn.gif' alt='send'><br><p class='animatedText'>User verified.</p>")
+                                                  $(window).keypress(function(e) {
+                                                    if (e.which === 32) {
+                                                      $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/486050/screenshots/2832783/seamless-payments.gif' alt='send'><br><br><p class='animatedText'>Processing payment</p>")
+                                                    }
+                                                    $(window).keypress(function(e) {
+                                                      if (e.which === 32) {
+                                                        $('.loader').html("<img src='https://d13yacurqjgara.cloudfront.net/users/760333/screenshots/2123456/lipco.gif' alt='send'><br><br><p class='animatedText'>Payment Complete!</p>")
+                                                      }
+                                                    })
+                                                    }
+                                                  })
+                                                }
+                                              })
+                                            }
+                                          })
+
+                                          }
+                                        })
+                                    }
+                                  })
+                              }
+                            })
+                        }
+                      })
+                    }
+                  })
                 }
-              });
+              })
             }
-          });
+          })
         }
-      });
+      })
     }
   })
-
 
 
 }
